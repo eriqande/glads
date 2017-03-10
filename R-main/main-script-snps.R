c163a81a18fe61dcb3287080b759d0110b6a1a41
Eric C. Anderson <eric.anderson@noaa.gov>
2017-03-10 10:45:32 -0800

    Update main-script-snps.R
    Most of these changes were made in December 2016, but Eric failed to commit them.  It runs through an example of using MACs to seed the simulation with random SNP variation. Then computes Fst using plink (still kinda slow) and then makes a little animation of the chromosome.
    
    The fitness function is a complete kluge.  Eric just hacked it together quickly, but almost no real thought went into it.

--- R-main/main-script-snps.R
+++ R-main/main-script-snps.R
@@ -18,30 +18,82 @@ library(progress)
 library(ggplot2)
 library(dplyr)
 
+
+# simulate some starting variation from the neutral coalescent (takes about a minute)
+# Going from around 1000 SNPs in 400 haploid genomes.
+chromo_length <- 1e8
+coal_start <- coalescent_starting_variation(n = 800, bp = chromo_length, S = 1000)
+
 # Higher level parameters that the user will set to run the simulation
 initial.population.size <- 200 # NOTE: THIS WILL BECOME A VECTOR TWO NUMBERS WHEN WE EXTEND TO TWO POPULATIONS
-n.loci                  <- 100 # how many linked genes we will deal with
-chromo_mb               <- 2e08
-loci.pos                <- sort(floor(runif(n.loci, min = 0, max = chromo_mb)))  # randomly sprinkle the loci along a chromo_mb Mb chromosom
-n.alleles.per.locus     <- 2   # we will assume that each locus has the same number of alleles to start with
-bv.for.alleles          <- t(array(1:5,c(n.alleles.per.locus,n.loci)))/4-0.25
-V.e                     <- 0.01 # standard deviation in environmental component of the phenotype
-n.gens                  <- 150 # number of generations.
+n.loci                  <- ncol(coal_start$mat) # how many linked genes we will deal with
+loci.pos                <- coal_start$pos
+n.alleles.per.locus     <- 2   # with snps everything is biallelic
+V.e                     <- 0.1 # standard deviation in environmental component of the phenotype
+n.gens                  <- 60 # number of generations.
 
-# a function to generate the initial population structure
-initial.struct          <- function(n.N1,n.l,n.a.l){ # n.N1 - initial N, n.l - N loci, n.a.l - alleles / locus
-  rand.ints             <- sample(1:n.a.l,n.N1*n.l*2,TRUE)
-  struct                <- array(rand.ints,c(n.N1,n.l,2)) # an array
-  return(struct)
+
+### NOW, I need:
+# 1. a function to turn coal_start into a a list of two population structs
+# 2. set the breeding values for all but a few loci (or just one locus) to zero.
+# 3. fix the fitness function so that each individual has a base relative fitness of
+#    one, say, and then we can add to that according to the presence of alleles at selected
+#    loci.
+# 4. Note that the fitness function has to be different for the two different populations!
+
+
+# This function takes the output of coalescent_starting_variation and puts it into a list of two structs.
+# it also sets the pop attribute to 1 or two on those structs, so that we know what type of fitness
+# function to put on it.  It also counts up the number of "2" alleles in each pop at each locus
+csv2struct <- function(C, n1) {
+  S <- C$mat
+  L <- ncol(S)
+  n2 <- nrow(S)/2 - n1
+  if(n2<=1) {
+    stop("n1 is too large")
+  }
+  s1 <- array(NA, dim = c(n1, L, 2))
+  s2 <- array(NA, dim = c(n2, L, 2))
+
+  s1[,,1] <- S[1:n1,]
+  s1[,,2] <- S[(n1 + 1):(2 * n1),]
+  s2[,,1] <- S[(2 * n1 + 1):(2 * n1 + n2),]
+  s2[,,2] <- S[(2 * n1 + n2 + 1):(2 * n1 + n2 + n2),]
+
+
+  list(
+    structs = list(s1, s2),
+    freqs1 = colSums(s1[,,1] == 2) + colSums(s1[,,2]==2),
+    freqs2 = colSums(s2[,,1] == 2) + colSums(s2[,,2]==2)
+  )
 }
 
-# estimate fitness
+
+tmp <- csv2struct(coal_start, initial.population.size)
+init.struct <- tmp$structs
+init.freqs <- tmp[2:3]
+
+# Now, we set the breeding values.  For now, let's say that there are three SNPs that occur in at least 1 copy,
+# but no more than 3 copies in population 2, as close as possible to the center of the chromosom,
+# that will confer additive efffects on the phenotype.
+candis <- which(init.freqs$freqs2 >= 3 & init.freqs$freqs2 <= 5)  # these are the indexes of the  candidate sites
+posis <- loci.pos[candis]  # these are their positions
+addis <- candis[order(abs(posis - chromo_length/2))[1]] # indexes of our three sites to use
+
+# now, every allele has a breeding value of 0 except for the "2" allele at those three
+# loci, which have a value of 1.  So an individual could ultimately build up to a value of 6
+# with three of those loci
+bv.for.alleles <- matrix(0, ncol = 2, nrow = n.loci)
+bv.for.alleles[addis, 2] <- 1
+
+
+
+# estimate fitness.  Eric has fully hacked this so that it depends only on z and b0 in a very simple way
+# I could set b0 to around 2 for population 2 and 0 for population 1
 fitness <- function(z,n,b0,b1,b2,b3,epsilon){
-  # so this is a bit of a wierd fitness function, but it should see if things work or not.
-  ze <- z[,1]
-  ze2 <- ze^2
-  w <- round(b0 + b1*ze + b2*ze2 + b3*n + rnorm(n,0,epsilon),0)
-  w <- ifelse(w<0,0,w)
+  ze <- z[,1] * b0
+  ze2 <- ze + 1
+  w <- rmultinom(n = 1, size = n * 2, prob = ze2/sum(ze2))
   return(w)
 }
 
@@ -81,8 +133,9 @@ doer <- function(x){
   b2 <- x[[7]]
   b3 <- x[[8]]
   epsil <- x[[9]]
-  zs             <- rcpp_g2p_map(struct, dim(struct), bvs, nloc, ve)
-  fits           <- cbind(zs,fitness(zs,dim(zs)[1],b0,b1,b2,b3,epsil))
+  zs             <- rcpp_g2p_map(struct, dim(struct), bvs, 0, ve)  # we set the number of loci to zero because we don't need to subtract them off
+  zs[,1]         <- abs(zs[,1])  # this is a hack here to accommodate Eric's silly fitness model.
+  fits           <- cbind(zs,fitness(zs,initial.population.size,b0,b1,b2,b3,epsil))
   pairs          <- mating(fits,struct)
   mums           <- pairs[[1]]
   dads           <- pairs[[2]]
@@ -92,17 +145,16 @@ doer <- function(x){
 #  struct.rt[,,1] <- rcpp_recombo_segregate(mums, dim(mums), rep(0.001, dim(mums)[2] - 1))
 #  struct.rt[,,2] <- rcpp_recombo_segregate(dads, dim(dads), rep(0.001, dim(dads)[2] - 1))
 
-  struct.rt[,,1] <- rcpp_recombo_segregate_expo(mums, dim(mums), loci.pos, chromo_mb)
-  struct.rt[,,2] <- rcpp_recombo_segregate_expo(dads, dim(dads), loci.pos, chromo_mb)
+  struct.rt[,,1] <- rcpp_recombo_segregate_expo(mums, dim(mums), loci.pos, chromo_length)
+  struct.rt[,,2] <- rcpp_recombo_segregate_expo(dads, dim(dads), loci.pos, chromo_length)
   return(struct.rt)
 }
 
 pb <- progress_bar$new(format = " Doing its shit [:bar] :percent eta: :eta",total =n.gens, clear = FALSE, width= 100)
-start.1 <- struct.1 <- start.2 <- struct.2 <- initial.struct(initial.population.size,n.loci,n.alleles.per.locus)
-
+struct.1 <- init.struct[[1]]
+struct.2 <- init.struct[[2]]
 res <- list()
 
-start.1 <- struct.1 <- start.2 <- struct.2 <- initial.struct(initial.population.size,n.loci,n.alleles.per.locus)
 
 
 # testing out the expy_recombo
@@ -115,8 +167,8 @@ res <- list()
 res[["0"]] <- list(struct.1, struct.2)
 
 for (i in 1:n.gens){
-  x1 <- list(struct.1,bv.for.alleles,n.loci,V.e,2,0.2,-0.005,-0.01,1)
-  x2 <- list(struct.2,bv.for.alleles,n.loci,V.e,1.5,0.1,-0.002,-0.005,1)
+  x1 <- list(struct.1,bv.for.alleles,n.loci,V.e,0,0.2,-0.005,-0.01,1)
+  x2 <- list(struct.2,bv.for.alleles,n.loci,V.e,10,0.1,-0.002,-0.005,1)
   x <- list(x1,x2)
   out <- mclapply(x,doer)
   struct.1 <- out[[1]]
@@ -124,7 +176,8 @@ for (i in 1:n.gens){
   outd <- fast_dispersal(struct.1,struct.2,0.01,0.01)
   struct.1 <- outd[[1]]
   struct.2 <- outd[[2]]
-  if (i %% 10==0) res[[paste(i)]] <- list(struct.1,struct.2)
+  #if (i %% 10==0) res[[paste(i)]] <- list(struct.1,struct.2)
+  res[[paste(i)]] <- list(struct.1,struct.2)
   pb$tick()
 }
 
@@ -165,31 +218,21 @@ mean(dist[,length(res)])*n.loci/2
 
 
 
-#### OLD USELESS FST CALC STUFF MARKED FOR REMOVAL ####
-# then compute Fst at every locus every 10 generations (that were stored)
-# (usig the pegas package....takes a ridiculous amount of time...silly!)
-fst_df <- lapply(res, function(x) fst_at_loci_with_pos(x[[1]], x[[2]], loci.pos)) %>%
-  bind_rows(.id = "generation") %>%
-  mutate(generation = as.numeric(generation))
-
-
-# plot the Fst values across loci on the last iteration
-last_gen <- fst_df %>%
-  filter(generation == max(generation))
-
-ggplot(last_gen, aes(x = pos, y = Fst)) +
-  geom_point() +
-  geom_line()
-
-
-
-#plot(1:200,as.vector(fst_df[fst_df$generation=='1500',3]))
 
 #### COMPUTE FST WITH PLINK ####
 fst_df <- lapply(res, function(x) fst_via_plink(x, loci.pos)) %>%
   dplyr::bind_rows(.id = "generation") %>%
   mutate(generation = as.numeric(generation))
 
-ggplot(fst_df, aes(x = POS, y = FST)) +
-  geom_line() +
-  facet_wrap(~generation)
+fst_df2 <- fst_df %>%
+  filter(!is.na(FST)) %>%
+  group_by(generation) %>%
+  mutate(dens = supsmu(POS, FST)$y)
+
+p <- ggplot(fst_df2, aes(x = POS, y = FST, frame = generation)) +
+  geom_point(size = 0.05) +
+  geom_line(mapping = aes(x = POS, y = 4*dens), colour = "red")
+
+library(gganimate)
+
+gg_animate(p)
